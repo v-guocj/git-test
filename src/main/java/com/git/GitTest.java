00a6a0b263c4d389ecb52f8a6e9a047eebf76982
guocj <g1124720929@163.com>
2022-06-19 16:43:21 +0800

    second commit

--- src/main/java/com/git/GitTest.java
+++ src/main/java/com/git/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hi git");
+        System.out.println("git");
     }
 }
